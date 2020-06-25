257a836cfab4b3d2d1d908e1c50b9c50eb2eb816
Meenu Sai <40272408+MeenuSai@users.noreply.github.com>
2020-06-25 17:03:39 +0530

    Update login.py

--- ConfigPage/login.py
+++ ConfigPage/login.py
@@ -45,7 +45,7 @@ def details():
     print("Got data")
     conn =pymysql.connect(database="bank",user="admin",password="admin",host="localhost")
     cur=conn.cursor()
-    cur.execute("INSERT INTO personal_details (name, ph.no) VALUES (%(name)s, %(mobile_number)s);",data)
+    cur.execute("INSERT INTO personal_details (name, ph) VALUES (%(name)s, %(mobile_number)s);",data)
     conn.commit
     conn.close()
     flash('Saved Successfully')
